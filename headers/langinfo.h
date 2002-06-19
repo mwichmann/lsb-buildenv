ee59e4dc668eb948e7200e456f5d6ea320469ec8
anderson <anderson>
2002-06-19 03:06:07 -0400

    Populate langinfo.h with enoug to allow apache to build again

--- headers/langinfo.h
+++ headers/langinfo.h
@@ -1,12 +1,100 @@
 #ifndef _LANGINFO_H_
 #define _LANGINFO_H_
 
+#include <nl_types.h>
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-#include <nl_types.h>
 
+
+
+
+
+/* LC_TIME category: date and time formatting.*/
+
+
+
+/* Abbreviated days of the week.*/
+#define ABDAY_1	0x20000
+#define ABDAY_2	0x20001
+#define ABDAY_3	0x20002
+#define ABDAY_4	0x20003
+#define ABDAY_5	0x20004
+#define ABDAY_6	0x20005
+#define ABDAY_7	0x20006
+
+
+
+/* Long-named days of the week.*/
+#define DAY_1	0x20007
+#define DAY_2	0x20008
+#define DAY_3	0x20009
+#define DAY_4	0x2000A
+#define DAY_5	0x2000B
+#define DAY_6	0x2000C
+#define DAY_7	0x2000D
+
+
+
+/* Abbreviated month names.*/
+#define ABMON_10	0x00217
+#define ABMON_1	0x2000E
+#define ABMON_2	0x2000F
+#define ABMON_3	0x20010
+#define ABMON_4	0x20011
+#define ABMON_5	0x20012
+#define ABMON_6	0x20013
+#define ABMON_7	0x20014
+#define ABMON_8	0x20015
+#define ABMON_9	0x20016
+#define ABMON_11	0x20018
+#define ABMON_12	0x20019
+
+
+
+/* Long month names.*/
+#define MON_1	0x2001A
+#define MON_2	0x2001B
+#define MON_3	0x2001C
+#define MON_4	0x2001D
+#define MON_5	0x2001E
+#define MON_6	0x2001F
+#define MON_7	0x20020
+#define MON_8	0x20021
+#define MON_9	0x20022
+#define MON_10	0x20023
+#define MON_11	0x20024
+#define MON_12	0x20025
+
+
+
+#define AM_STR	0x20026
+#define PM_STR	0x20027
+
+
+
+
+
+
+
+
+
+/*  LC_COLLATE category: text sorting. This information is accessed by the strcoll and strxfrm functions. These `nl_langinfo' names are used only internally.*/
+
+
+
+/* LC_CTYPE category: character classification. This information is accessed by the functions in <ctype.h>.These 'nl_langinfo' names are used only internally.*/
+#define CODESET	14
+
+
+
+/* LC_MONETARY category: formatting of monetary quantities. These items each correspond to a member of `struct lconv', defined in <locale.h>.*/
+
+
+
+/* LC_NUMERIC category: formatting of numbers. These also correspond to members of `struct lconv'; see <locale.h>.*/
 
 
 
