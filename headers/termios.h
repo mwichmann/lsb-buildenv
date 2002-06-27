7afdd0099386a0c9a9bc61f4c72f701943bbc084
anderson <anderson>
2002-06-26 22:39:36 -0400

    Add missing struct winsize

--- headers/termios.h
+++ headers/termios.h
@@ -110,6 +110,15 @@ extern "C"
 #endif
 
 
+  struct winsize
+  {
+    unsigned short ws_row;
+    unsigned short ws_col;
+    unsigned short ws_xpixel;
+    unsigned short ws_ypixel;
+  }
+   ;
+
 
 
 
