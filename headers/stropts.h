1c844f04e7f00eb1660a3fb7e8976916a7ded58b
anderson <anderson>
2002-08-09 00:19:18 -0400

    Empty header to make the world a safer place

---
+++ headers/stropts.h
@@ -0,0 +1,22 @@
+#ifndef _STROPTS_H_
+#define _STROPTS_H_
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+
+
+/* The streams interfaces are excluded from the LSB.
+This empty file is needed to make the compilation environment happy.*/
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
