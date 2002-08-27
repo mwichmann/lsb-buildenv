5aca54e20e6fe3bcdc49f933eb7d528d99fce7f6
anderson <anderson>
2002-08-27 17:25:21 -0400

    Fill in the comments from the Zlib spec

--- headers/zlib.h
+++ headers/zlib.h
@@ -57,20 +57,20 @@ extern "C"
 
   typedef struct z_stream_s
   {
-    Bytef *next_in;
-    uInt avail_in;
-    uLong total_in;
-    Bytef *next_out;
-    uInt avail_out;
-    uLong total_out;
-    char *msg;
-    struct internal_state *state;
-    alloc_func zalloc;
-    free_func zfree;
-    voidpf opaque;
-    int data_type;
-    uLong adler;
-    uLong reserved;
+    Bytef *next_in;		/* next input byte */
+    uInt avail_in;		/* number of bytes available at next_in */
+    uLong total_in;		/* total nb of input bytes read so far */
+    Bytef *next_out;		/* next output byte should be put there */
+    uInt avail_out;		/* remaining free space at next_out */
+    uLong total_out;		/* total nb of bytes output so far */
+    char *msg;			/* last error message, NULL if no error */
+    struct internal_state *state;	/* not visible by applications */
+    alloc_func zalloc;		/* used to allocate the internal state */
+    free_func zfree;		/* used to free the internal state */
+    voidpf opaque;		/* private data object passed to zalloc and zfree */
+    int data_type;		/* best guess about the data type: ascii or binary */
+    uLong adler;		/* adler32 value of the uncompressed data */
+    uLong reserved;		/* reserved for future use */
   }
   z_stream;
 
