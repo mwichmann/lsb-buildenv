3b7c84930e9b74e5d169f79062dbaa653d89e320
anderson <anderson>
2002-09-18 20:59:34 -0400

    Picked up 2 mor eI18n interfaces

--- headers/libintl.h
+++ headers/libintl.h
@@ -16,11 +16,13 @@ extern "C"
   extern char *dcgettext (const char *, const char *, int);
   extern char *dgettext (const char *, const char *);
   extern char *gettext (const char *);
+  extern char *textdomain (const char *);
   extern char *bind_textdomain_codeset (const char *, const char *);
   extern char *dcngettext (const char *, const char *, const char *,
 			   unsigned long, int);
   extern char *dngettext (const char *, const char *, const char *,
 			  unsigned long);
+  extern char *ngettext (const char *, const char *, unsigned long);
 #ifdef __cplusplus
 }
 #endif
