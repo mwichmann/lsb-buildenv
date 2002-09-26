7c6e71674f0d04b29e261e8b92c2ddede714b79c
anderson <anderson>
2002-09-26 18:02:05 -0400

    Don't include prototypes for deprecated functions

--- headers/dirent.h
+++ headers/dirent.h
@@ -47,7 +47,6 @@ extern "C"
    ;
 
 
-  extern int alphasort (struct dirent **, struct dirent **);
   extern void rewinddir (DIR *);
   extern void seekdir (DIR *, long);
   extern long telldir (DIR *);
