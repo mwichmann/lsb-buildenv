64123920dbe6ba419bce8ce902e573e2046537ec
anderson <anderson>
2002-09-30 19:34:44 -0400

    Add the addrinfo struct now that we have the gai interfaces

--- headers/sys/socket.h
+++ headers/sys/socket.h
@@ -50,6 +50,19 @@ extern "C"
   }
    ;
 
+  struct addrinfo
+  {
+    int ai_flags;
+    int ai_family;
+    int ai_socktype;
+    int ai_protocol;
+    size_t ai_addrlen;
+    struct sockaddr *ai_addr;
+    char *ai_canonname;
+    struct addrinfo *ai_next;
+  }
+   ;
+
 
 
 
