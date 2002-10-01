aade9b63830370b531b22836e7d8206e9adc3792
anderson <anderson>
2002-09-30 20:48:09 -0400

    A couple more const's needed

--- headers/security/pam_appl.h
+++ headers/security/pam_appl.h
@@ -103,8 +103,8 @@ extern "C"
 
 
 
-  extern int pam_set_item (pam_handle_t *, int, void *);
-  extern int pam_get_item (pam_handle_t *, int, void **);
+  extern int pam_set_item (pam_handle_t *, int, const void *);
+  extern int pam_get_item (pam_handle_t *, int, const void **);
   extern char *pam_strerror (pam_handle_t *, int);
   extern char **pam_getenvlist (pam_handle_t *);
   extern int pam_fail_delay (pam_handle_t *, unsigned int);
