c98abec670286a8bef9ce0b78abfdf56b0a5784a
anderson <anderson>
2002-10-27 17:16:55 -0400

    Add rpc/types.h

---
+++ headers/rpc/types.h
@@ -0,0 +1,21 @@
+#ifndef _RPC_TYPES_H_
+#define _RPC_TYPES_H_
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+
+  typedef int bool_t;
+
+  typedef int enum_t;
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
