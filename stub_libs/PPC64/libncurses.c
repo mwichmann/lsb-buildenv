094c100640384ac451bf0f094f2351ee164ce0df
cyeoh <cyeoh>
2002-11-01 07:39:59 -0400

    Fixup PPC64 stub libraries

--- stub_libs/PPC64/libncurses.c
+++ stub_libs/PPC64/libncurses.c
@@ -271,5 +271,5 @@ void wtouchln(void) {} ;
 void wvline(void) {} ;
 __asm__(".globl COLS; .data; .type COLS,@object; .size COLS, 4; COLS: .long 0");
 __asm__(".globl LINES; .data; .type LINES,@object; .size LINES, 4; LINES: .long 0");
-__asm__(".globl curscr; .data; .type curscr,@object; .size curscr, 0; curscr: .long 0");
-__asm__(".globl stdscr; .data; .type stdscr,@object; .size stdscr, 0; stdscr: .long 0");
+__asm__(".globl curscr; .data; .type curscr,@object; .size curscr, 8; curscr: .long 0");
+__asm__(".globl stdscr; .data; .type stdscr,@object; .size stdscr, 8; stdscr: .long 0");
