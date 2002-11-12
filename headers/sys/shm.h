c893398ef4f74d2e6497dbed1d43730d959df39f
cyeoh <cyeoh>
2002-11-12 09:14:05 -0400

    resync with db

--- headers/sys/shm.h
+++ headers/sys/shm.h
@@ -51,6 +51,11 @@ extern "C"
   typedef unsigned long shmatt_t;
 
 #endif
+#if __s390__
+/* S390 */
+  typedef unsigned long shmatt_t;
+
+#endif
 
 
 
@@ -105,12 +110,33 @@ extern "C"
     unsigned int __unused3;
     time_t shm_ctime;
     unsigned int __unused4;
-    size_t shm_segz;
+    size_t shm_segsz;
     pid_t shm_cpid;
     pid_t shm_lpid;
     shmatt_t shm_nattch;
     unsigned long __unused5;
     unsigned long __unused6;
+  }
+   ;
+
+#endif
+#if __s390__
+/* S390 */
+  struct shmid_ds
+  {
+    struct ipc_perm shm_perm;
+    size_t shm_segsz;
+    time_t shm_atime;
+    unsigned long __unused1;
+    time_t shm_dtime;
+    unsigned long __unused2;
+    time_t shm_ctime;
+    unsigned long __unused3;
+    pid_t shm_cpid;
+    pid_t shm_lpid;
+    shmatt_t shm_nattch;
+    unsigned long __unused4;
+    unsigned long __unused5;
   }
    ;
 
