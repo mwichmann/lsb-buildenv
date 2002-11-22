239e7ff0f16fc091652dca26a916d3cc4127d752
anderson <anderson>
2002-11-22 03:42:50 -0400

    Fill in statvfs* for PPC32

--- headers/sys/statvfs.h
+++ headers/sys/statvfs.h
@@ -93,6 +93,48 @@ extern "C"
    ;
 
 #endif
+#if __powerpc__ && !__powerpc64__
+/* PPC32 */
+  struct statvfs
+  {
+    unsigned long f_bsize;
+    unsigned long f_frsize;
+    fsblkcnt_t f_blocks;
+    fsblkcnt_t f_bfree;
+    fsblkcnt_t f_bavail;
+    fsfilcnt_t f_files;
+    fsfilcnt_t f_ffree;
+    fsfilcnt_t f_favail;
+    unsigned long f_fsid;
+    int __f_unused;
+    unsigned long f_flag;
+    unsigned long f_namemax;
+    int __f_spare[6];
+  }
+   ;
+
+#endif
+#if __powerpc__ && !__powerpc64__
+/* PPC32 */
+  struct statvfs64
+  {
+    unsigned long f_bsize;
+    unsigned long f_frsize;
+    fsblkcnt64_t f_blocks;
+    fsblkcnt64_t f_bfree;
+    fsblkcnt64_t f_bavail;
+    fsfilcnt64_t f_files;
+    fsfilcnt64_t f_ffree;
+    fsfilcnt64_t f_favail;
+    unsigned long f_fsid;
+    int __f_unused;
+    unsigned long f_flag;
+    unsigned long f_namemax;
+    int __f_spare[6];
+  }
+   ;
+
+#endif
 
   extern int fstatvfs (int, struct statvfs *);
   extern int fstatvfs64 (int, struct statvfs64 *);
