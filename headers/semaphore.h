5049264b31b9540892cd875d43301f5ff75cbb10
anderson <anderson>
2002-12-03 03:39:38 -0400

    Add missing const in prototypes

--- headers/semaphore.h
+++ headers/semaphore.h
@@ -38,10 +38,10 @@ extern "C"
   extern int sem_destroy (sem_t *);
   extern int sem_getvalue (sem_t *, int *);
   extern int sem_init (sem_t *, int, unsigned int);
-  extern sem_t *sem_open (char *, int, ...);
+  extern sem_t *sem_open (const char *, int, ...);
   extern int sem_post (sem_t *);
   extern int sem_trywait (sem_t *);
-  extern int sem_unlink (char *);
+  extern int sem_unlink (const char *);
   extern int sem_wait (sem_t *);
   extern int sem_timedwait (void);
 #ifdef __cplusplus
