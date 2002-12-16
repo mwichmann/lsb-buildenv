69c92bc100bae871b471a4a64a7c76ad46a9280d
anderson <anderson>
2002-12-15 22:57:25 -0400

    Add locale_t for C++ support

--- headers/locale.h
+++ headers/locale.h
@@ -61,6 +61,27 @@ extern "C"
 
 
 
+
+
+  typedef struct __locale_struct
+  {
+    struct locale_data *__locales[13];
+    const unsigned short *__ctype_b;
+    const int *__ctype_tolower;
+    const int *__ctype_toupper;
+    const char *__names[13];
+  }
+   *__locale_t;
+
+
+
+
+
+
+
+  typedef __locale_t locale_t;
+
+
   extern struct lconv *localeconv (void);
   extern char *setlocale (int, char *);
 #ifdef __cplusplus
