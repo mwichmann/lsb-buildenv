7d72adf5bda19f63b62a4e4557b4765fca5d8baf
anderson <anderson>
2003-02-24 14:14:27 -0400

    Add features.h

---
+++ headers/features.h
@@ -0,0 +1,38 @@
+#ifndef _FEATURES_H_
+#define _FEATURES_H_
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+
+
+#define _ISOC99_SOURCE	1
+#define __STDC_ISO_10646__	1
+#define __USE_BSD	1
+#define __USE_ISOC99	1
+#define __USE_LARGEFILE	1
+#define __USE_LARGEFILE64	1
+#define __USE_MISC	1
+#define __USE_POSIX	1
+#define __USE_POSIX199309	1
+#define __USE_POSIX199506	1
+#define __USE_POSIX2	1
+#define __USE_REENTRANT	1
+#define __USE_SVID	1
+#define __USE_UNIX98	1
+#define __USE_XOPEN	1
+#define __USE_XOPEN_EXTENDED	1
+#define _POSIX_C_SOURCE	199506L
+#define _XOPEN_SOURCE	500
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
