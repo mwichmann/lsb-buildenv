21646a3ee0661a2a82b4ed9d2270cec97e51b92a
anderson <anderson>
2003-06-27 16:24:24 -0400

    curses additions

--- headers/term.h
+++ headers/term.h
@@ -11,18 +11,22 @@ extern "C"
 
 
 
-  extern int putp (char *);
+  extern int putp (const char *);
   extern int tigetflag (char *);
   extern int tigetnum (char *);
   extern char *tigetstr (char *);
   extern char *tparm (char *, ...);
-  extern int setupterm (const char *, int, int *);
+  extern TERMINAL *set_curterm (TERMINAL *);
+  extern int del_curterm (TERMINAL *);
+  extern int restartterm (char *, int, int *);
+  extern int setupterm (char *, int, int *);
   extern char *tgetstr (char *, char **);
   extern char *tgoto (char *, int, int);
   extern int tgetent (char *, const char *);
   extern int tgetflag (char *);
   extern int tgetnum (char *);
   extern int tputs (const char *, int, int (*putcproc) (int));
+  extern TERMINAL *cur_term;
 #ifdef __cplusplus
 }
 #endif
