b6bc531a9bbfb16ed0a1bcc83b70123d93616379
anderson <anderson>
2003-08-21 15:45:07 -0400

    Make int64_t architecture sensitive

--- headers/sys/types.h
+++ headers/sys/types.h
@@ -23,8 +23,11 @@ extern "C"
 
   typedef int int32_t;
 
+#if __i386__
+/* IA32 */
   typedef long long int64_t;
 
+#endif
   typedef unsigned char u_int8_t;
 
   typedef unsigned short u_int16_t;
@@ -94,6 +97,36 @@ extern "C"
 #if __s390x__
 /* S390X */
   typedef int64_t ssize_t;
+
+#endif
+#if __ia64__
+/* IA64 */
+  typedef long int64_t;
+
+#endif
+#if __powerpc64__
+/* PPC64 */
+  typedef long int64_t;
+
+#endif
+#if __x86_64__
+/* x86-64 */
+  typedef long int64_t;
+
+#endif
+#if __s390x__
+/* S390X */
+  typedef long int64_t;
+
+#endif
+#if __powerpc__ && !__powerpc64__
+/* PPC32 */
+  typedef long long int64_t;
+
+#endif
+#if __s390__ && !__s390x__
+/* S390 */
+  typedef long long int64_t;
 
 #endif
 
