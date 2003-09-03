7affea4b2a99cb53a8995cce9edd86ead01a42d0
anderson <anderson>
2003-09-03 17:33:40 -0400

    Fix the types used for msgqid on s390x

--- headers/sys/msg.h
+++ headers/sys/msg.h
@@ -162,14 +162,14 @@ extern "C"
   struct msqid_ds
   {
     struct ipc_perm msg_perm;
-    __time_t msg_stime;
-    __time_t msg_rtime;
-    __time_t msg_ctime;
+    time_t msg_stime;
+    time_t msg_rtime;
+    time_t msg_ctime;
     unsigned long __msg_cbytes;
     msgqnum_t msg_qnum;
     msglen_t msg_qbytes;
-    __pid_t msg_lspid;
-    __pid_t msg_lrpid;
+    pid_t msg_lspid;
+    pid_t msg_lrpid;
     unsigned long __unused4;
     unsigned long __unused5;
   }
