0d62df46852f64b6985960067c903c000a441465
anderson <anderson>
2003-12-11 17:26:03 -0400

    The _Unwind*() prototypes get dropped because they are no longer of type 'All'

--- headers/unwind.h
+++ headers/unwind.h
@@ -84,21 +84,6 @@ extern "C"
   typedef int _Unwind_Action;
 
 
-  extern void _Unwind_DeleteException (struct _Unwind_Exception *);
-  extern fde *_Unwind_Find_FDE (void);
-  extern _Unwind_Ptr _Unwind_ForcedUnwind (void);
-  extern _Unwind_Ptr _Unwind_GetDataRelBase (struct _Unwind_Context *);
-  extern _Unwind_Word _Unwind_GetGR (struct _Unwind_Context *, int);
-  extern _Unwind_Ptr _Unwind_GetIP (struct _Unwind_Context *);
-  extern _Unwind_Ptr _Unwind_GetLanguageSpecificData (struct _Unwind_Context
-						      *, unsigned int);
-  extern _Unwind_Ptr _Unwind_GetRegionStart (struct _Unwind_Context *);
-  extern _Unwind_Ptr _Unwind_GetTextRelBase (struct _Unwind_Context);
-  extern _Unwind_Reason_Code _Unwind_RaiseException (struct _Unwind_Exception
-						     *);
-  extern void _Unwind_Resume (struct _Unwind_Exception *);
-  extern void _Unwind_SetGR (struct _Unwind_Context *, int, unsigned int);
-  extern void _Unwind_SetIP (struct _Unwind_Context *, unsigned int);
 #ifdef __cplusplus
 }
 #endif
