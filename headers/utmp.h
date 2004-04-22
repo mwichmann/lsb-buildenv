b590895583c9a49ca107f1dd067af39209ca460e
anderson <anderson>
2004-04-22 19:38:51 -0400

    Fix time entry in lastlog for S390X and IA64 as they are 32-on-64 platforms also

--- headers/utmp.h
+++ headers/utmp.h
@@ -40,7 +40,7 @@ extern "C"
 /* IA64 */
   struct lastlog
   {
-    time_t ll_time;
+    int32_t ll_time;
     char ll_line[UT_LINESIZE];
     char ll_host[UT_HOSTSIZE];
   }
@@ -95,7 +95,7 @@ extern "C"
 /* S390X */
   struct lastlog
   {
-    time_t ll_time;
+    int32_t ll_time;
     char ll_line[UT_LINESIZE];
     char ll_host[UT_HOSTSIZE];
   }
