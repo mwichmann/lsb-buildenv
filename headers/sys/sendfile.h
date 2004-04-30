f53e865c1c47ca59fa8537eea0e0401db89447aa
anderson <anderson>
2004-04-30 15:09:17 -0400

    version produced by script changes

--- headers/sys/sendfile.h
+++ headers/sys/sendfile.h
@@ -1,12 +1,17 @@
 #ifndef _SYS_SENDFILE_H_
 #define _SYS_SENDFILE_H_
 
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#error Not Available in LSB
+#error "This header not permitted by the LSB"
+
+
+
+
 
 #ifdef __cplusplus
 }
