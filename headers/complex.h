a0f563699983fb5dd61539920ef7ea1cb96a8a05
anderson <anderson>
2004-05-12 21:54:53 -0400

    Fix complex math prototypes

--- headers/complex.h
+++ headers/complex.h
@@ -1,6 +1,7 @@
 #ifndef _COMPLEX_H_
 #define _COMPLEX_H_
 
+#include <math.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -11,74 +12,74 @@ extern "C"
 
 
 
-  extern double cabs (void);
-  extern float cabsf (float);
-  extern long double cabsl (long double);
-  extern double cacos (void);
-  extern float cacosf (float);
-  extern double cacosh (void);
-  extern float cacoshf (float);
-  extern long double cacoshl (long double);
-  extern long double cacosl (long double);
-  extern double carg (void);
-  extern float cargf (float);
-  extern long double cargl (long double);
-  extern double casin (void);
-  extern float casinf (float);
-  extern double casinh (void);
-  extern float casinhf (float);
-  extern long double casinhl (long double);
-  extern long double casinl (long double);
-  extern double catan (void);
-  extern float catanf (float);
-  extern double catanh (void);
-  extern float catanhf (float);
-  extern long double catanhl (long double);
-  extern long double catanl (long double);
-  extern double ccos (void);
-  extern float ccosf (float);
-  extern double ccosh (void);
-  extern float ccoshf (float);
-  extern long double ccoshl (long double);
-  extern long double ccosl (long double);
-  extern double cexp (void);
-  extern float cexpf (float);
-  extern long double cexpl (long double);
-  extern double cimag (void);
-  extern float cimagf (float);
-  extern long double cimagl (long double);
-  extern double clog (void);
-  extern float clog10f (float);
-  extern long double clog10l (long double);
-  extern float clogf (float);
-  extern long double clogl (long double);
-  extern double conj (void);
-  extern float conjf (float);
-  extern long double conjl (long double);
-  extern double cpow (void);
-  extern float cpowf (float, float);
-  extern long double cpowl (long double, long double);
-  extern double cproj (void);
-  extern float cprojf (float);
-  extern long double cprojl (long double);
-  extern double creal (void);
-  extern float crealf (float);
-  extern long double creall (long double);
-  extern double csin (void);
-  extern float csinf (float);
-  extern double csinh (void);
-  extern float csinhf (float);
-  extern long double csinhl (long double);
-  extern long double csinl (long double);
-  extern double csqrt (void);
-  extern float csqrtf (float);
-  extern long double csqrtl (long double);
-  extern double ctan (void);
-  extern float ctanf (float);
-  extern double ctanh (void);
-  extern float ctanhf (float);
-  extern long double ctanhl (long double);
-  extern long double ctanl (long double);
+  extern double cabs (double complex);
+  extern float cabsf (float complex);
+  extern long double cabsl (long double complex);
+  extern double complex cacos (double complex);
+  extern float complex cacosf (float complex);
+  extern double complex cacosh (double complex);
+  extern float complex cacoshf (float complex);
+  extern long double complex cacoshl (long double complex);
+  extern long double complex cacosl (long double complex);
+  extern double carg (double complex);
+  extern float cargf (float complex);
+  extern long double cargl (long double complex);
+  extern double complex casin (double complex);
+  extern float complex casinf (float complex);
+  extern double complex casinh (double complex);
+  extern float complex casinhf (float complex);
+  extern long double complex casinhl (long double complex);
+  extern long double complex casinl (long double complex);
+  extern double complex catan (double complex);
+  extern float complex catanf (float complex);
+  extern double complex catanh (double complex);
+  extern float complex catanhf (float complex);
+  extern long double complex catanhl (long double complex);
+  extern long double complex catanl (long double complex);
+  extern double complex ccos (double complex);
+  extern float complex ccosf (float complex);
+  extern double complex ccosh (double complex);
+  extern float complex ccoshf (float complex);
+  extern long double complex ccoshl (long double complex);
+  extern long double complex ccosl (long double complex);
+  extern double complex cexp (double complex);
+  extern float complex cexpf (float complex);
+  extern long double complex cexpl (long double complex);
+  extern double cimag (double complex);
+  extern float cimagf (float complex);
+  extern long double cimagl (long double complex);
+  extern double complex clog (double complex);
+  extern float complex clog10f (float complex);
+  extern long double complex clog10l (long double complex);
+  extern float complex clogf (float complex);
+  extern long double complex clogl (long double complex);
+  extern double complex conj (double complex);
+  extern float complex conjf (float complex);
+  extern long double complex conjl (long double complex);
+  extern double complex cpow (double complex, double complex);
+  extern float complex cpowf (float complex, float complex);
+  extern long double complex cpowl (long double complex, long double complex);
+  extern double complex cproj (double complex);
+  extern float complex cprojf (float complex);
+  extern long double complex cprojl (long double complex);
+  extern double creal (double complex);
+  extern float crealf (float complex);
+  extern long double creall (long double complex);
+  extern double complex csin (double complex);
+  extern float complex csinf (float complex);
+  extern double complex csinh (double complex);
+  extern float complex csinhf (float complex);
+  extern long double complex csinhl (long double complex);
+  extern long double complex csinl (long double complex);
+  extern double complex csqrt (double complex);
+  extern float complex csqrtf (float complex);
+  extern long double complex csqrtl (long double complex);
+  extern double complex ctan (double complex);
+  extern float complex ctanf (float complex);
+  extern double complex ctanh (double complex);
+  extern float complex ctanhf (float complex);
+  extern long double complex ctanhl (long double complex);
+  extern long double complex ctanl (long double complex);
 #ifdef __cplusplus
 }
 #endif
