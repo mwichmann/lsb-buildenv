34ee03bb72965dae151c1f94950e6a3764675422
anderson <anderson>
2004-07-13 23:02:27 -0400

    Pick up struct timex, which should have benn there all along

--- headers/sys/timex.h
+++ headers/sys/timex.h
@@ -12,6 +12,42 @@ extern "C"
 
 
 
+  struct timex
+  {
+    unsigned int modes;
+    long offset;
+    long freq;
+    long maxerror;
+    long esterror;
+    int status;
+    long constant;
+    long precision;
+    long tolerance;
+    struct timeval time;
+    long tick;
+    long ppsfreq;
+    long jitter;
+    int shift;
+    long stabil;
+    long jitcnt;
+    long calcnt;
+    long errcnt;
+    long stbcnt;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+    int:32;
+  }
+   ;
+
 
   extern int adjtimex (struct timex *);
 #ifdef __cplusplus
