a5d26206d913eecef6719014e186feecd93c2c1b
anderson <anderson>
2004-07-15 12:41:35 -0400

    adjtimex is deprecated, so we don't need struct timex no

--- headers/sys/timex.h
+++ headers/sys/timex.h
@@ -1,8 +1,6 @@
 #ifndef _SYS_TIMEX_H_
 #define _SYS_TIMEX_H_
 
-#include <sys/types.h>
-#include <sys/time.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -11,42 +9,6 @@ extern "C"
 
 
 
-
-  struct timex
-  {
-    unsigned int modes;
-    long offset;
-    long freq;
-    long maxerror;
-    long esterror;
-    int status;
-    long constant;
-    long precision;
-    long tolerance;
-    struct timeval time;
-    long tick;
-    long ppsfreq;
-    long jitter;
-    int shift;
-    long stabil;
-    long jitcnt;
-    long calcnt;
-    long errcnt;
-    long stbcnt;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-    int:32;
-  }
-   ;
 
 
 #ifdef __cplusplus
