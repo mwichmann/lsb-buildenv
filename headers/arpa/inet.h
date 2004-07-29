f1766f02d84f3b93c5a5d67b6f78b46630c473bf
anderson <anderson>
2004-07-29 15:05:25 -0400

    Fix prototype for inet_ntop

--- headers/arpa/inet.h
+++ headers/arpa/inet.h
@@ -2,7 +2,7 @@
 #define _ARPA_INET_H_
 
 #include <inttypes.h>
-#include <stddef.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 
 #ifdef __cplusplus
@@ -16,7 +16,7 @@ extern "C"
 
   extern in_addr_t inet_addr (const char *);
   extern char *inet_ntoa (struct in_addr);
-  extern const char *inet_ntop (int, const void *, char *, size_t);
+  extern const char *inet_ntop (int, const void *, char *, socklen_t);
   extern int inet_pton (int, const char *, void *);
 #ifdef __cplusplus
 }
