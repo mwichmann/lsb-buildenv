f3ffeeb1f8720608cdf20cbb1c06a95a4700bb01
anderson <anderson>
2005-03-22 13:58:40 -0400

    Put struct __locale_struct & __locale_t back in to make C++ happy

--- headers/locale.h
+++ headers/locale.h
@@ -64,13 +64,23 @@ extern "C"
 
 
 
+  typedef struct __locale_struct
+  {
+    struct locale_data *__locales[13];
+    const unsigned short *__ctype_b;
+    const int *__ctype_tolower;
+    const int *__ctype_toupper;
+    const char *__names[13];
+  }
+   *__locale_t;
 
 
 
 
 
 
-  typedef void *locale_t;
+
+  typedef struct __locale_struct *locale_t;
 
 
 /* These are the bits that can be set in the CATEGORY_MASK argument to newlocale().*/
