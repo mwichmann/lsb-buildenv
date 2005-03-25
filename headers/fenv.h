f00cff42a8d5112138e054ad6b72f5553db0da74
anderson <anderson>
2005-03-24 23:09:17 -0400

    Get the fenv_t right for x86-64

--- headers/fenv.h
+++ headers/fenv.h
@@ -301,7 +301,24 @@ extern "C"
 #endif
 #if __x86_64__
 /* x86-64 */
-  typedef unsigned int fenv_t;
+  typedef struct
+  {
+    unsigned short __control_word;
+    unsigned short __unused1;
+    unsigned short __status_word;
+    unsigned short __unused2;
+    unsigned short __tags;
+    unsigned short __unused3;
+    unsigned int __eip;
+    unsigned short __cs_selector;
+    unsigned int __opcode:11;
+    unsigned int __unused4:5;
+    unsigned int __data_offset;
+    unsigned short __data_selector;
+    unsigned short __unused5;
+    unsigned int __mxcsr;
+  }
+  fenv_t;
 
 #endif
 
