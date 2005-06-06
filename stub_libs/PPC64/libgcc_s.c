f2ec8d8e1fed01c2119fa4a0393ab597c6a53839
anderson <anderson>
2005-06-06 18:55:28 -0400

    Bug 899 - add missing _Unwind functions

--- stub_libs/PPC64/libgcc_s.c
+++ stub_libs/PPC64/libgcc_s.c
@@ -1,6 +1,9 @@
+void _Unwind_Backtrace() {} ;
 void _Unwind_DeleteException() {} ;
+void _Unwind_FindEnclosingFunction() {} ;
 void _Unwind_Find_FDE() {} ;
 void _Unwind_ForcedUnwind() {} ;
+void _Unwind_GetBSP() {} ;
 void _Unwind_GetDataRelBase() {} ;
 void _Unwind_GetGR() {} ;
 void _Unwind_GetIP() {} ;
@@ -9,5 +12,6 @@ void _Unwind_GetRegionStart() {} ;
 void _Unwind_GetTextRelBase() {} ;
 void _Unwind_RaiseException() {} ;
 void _Unwind_Resume() {} ;
+void _Unwind_Resume_or_Rethrow() {} ;
 void _Unwind_SetGR() {} ;
 void _Unwind_SetIP() {} ;
