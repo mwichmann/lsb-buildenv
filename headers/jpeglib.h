3125687a1fdc4578b1f8d5e84c478593537f11a2
Mats Wichmann <mats@freestandards.org>
2006-10-09 16:50:30 -0600

    regenerate jpeglib headers for bug 1493 DB change

--- headers/jpeglib.h
+++ headers/jpeglib.h
@@ -11,8 +11,6 @@ extern "C" {
 
 #define FAR
 #define HAVE_PROTOTYPES
-#define HAVE_STDDEF_H
-#define HAVE_STDLIB_H
 #define HAVE_UNSIGNED_CHAR
 #define HAVE_UNSIGNED_SHORT
 #define jpeg_create_compress(cinfo)	 \
