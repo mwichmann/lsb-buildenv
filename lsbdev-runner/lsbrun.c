8e989829798e5004d88fba2dd05d7456e34a42ba
Jeff Licquia <jeff@licquia.org>
2007-05-18 16:01:24 -0400

    First shot at lsbrun, a helper for running LSB apps on a non-LSB system.

---
+++ lsbdev-runner/lsbrun.c
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2007 The Linux Foundation.
+ *
+ * Originally written by Jeff Licquia <licquia@linux-foundation.org>.
+ */
+
+#include <limits.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <libgen.h>
+
+/* The full paths to the LSB and native dynamic linkers. */
+
+#if   __i386__
+
+const char *lsb_linker_path = "/lib/ld-lsb.so.3";
+const char *native_linker_path = "/lib/ld-linux.so.2";
+
+#elif __powerpc__ && !__powerpc64__
+
+const char *lsb_linker_path = "/lib/ld-lsb-ppc32.so.3";
+
+#elif __powerpc64__
+
+const char *lsb_linker_path = "/lib64/ld-lsb-ppc64.so.3";
+
+#elif __ia64__
+
+const char *lsb_linker_path = "/lib/ld-lsb-ia64.so.3";
+
+#elif __s390__ && !__s390x__
+
+const char *lsb_linker_path = "/lib/ld-lsb-s390.so.3";
+
+#elif __s390x__
+
+const char *lsb_linker_path = "/lib64/ld-lsb-s390x.so.3";
+
+#elif __x86_64__
+
+const char *lsb_linker_path = "/lib64/ld-lsb-x86-64.so.3";
+
+#else
+#error Current architecture not supported.
+#endif
+
+/* Take a non-absolute path and make it absolute.  If this is a bare
+   filename, search the PATH. */
+
+const char *find_binary_path(const char *binary)
+{
+}
+
+void help(FILE *stream)
+{
+  fputs("help goes here\n", stream);
+}
+
+int main(int argc, char *argv[])
+{
+  char **real_cmdline;
+  char **new_cmdline;
+  int real_cmdline_len;
+
+  const char *linker_path;
+  const char *fixed_exec;
+  char current_dir[PATH_MAX];
+
+  char buf[PATH_MAX];
+  int index;
+
+  /* First, let's figure out how we were started. */
+
+  if (strstr(argv[0], "lsbrun") != NULL) {
+    if ((argc < 2) || (strcmp(argv[1], "--help") == 0)) {
+      help(stdout);
+      exit(0);
+    } else {
+      real_cmdline = &(argv[1]);
+  } else {
+
+    /* Copy argv. */
+
+    real_cmdline = (char **)malloc(sizeof(char *) * (argc + 2));
+    for (index = 0; index < argc; index++)
+      real_cmdline[index] = argv[index];
+    real_cmdline[argc] = NULL;
+
+    /* When started as a proxy to a binary, the real binary will be
+       stored next to this program, but with a '.' prepended.  Look
+       for that binary. */
+
+    strcpy(buf, argv[0], PATH_MAX);
+    exec_fn = strdup(basename(buf));
+
+    if (access(argv[0], X_OK) == 0) {
+      strcpy(buf, argv[0], PATH_MAX);
+      abs_exec_dir = strdup(dirname(buf));
+    } else {
+      abs_exec_dir = find_binary_path(argv[0]);
+    }
+
+    realloc(exec_fn, strlen(exec_fn) + 2);
+    memmove(exec_fn + 1, exec_fn, strlen(exec_fn) + 1);
+    exec_fn[0] = '.';
+
+    snprintf(buf, PATH_MAX, "%s/%s", abs_exec_dir, exec_fn);
+
+    /* Otherwise, set this as the real executable to run. */
+    real_cmdline[0] = strdup(buf);
+
+    /* Clean up. */
+    free(exec_fn);
+    free(abs_exec_dir);
+  }
+
+  /* Fail if the real executable isn't right. */
+
+  if (access(real_cmdline[0], X_OK) != 0) {
+    fprintf(stderr, "lsbrun: could not execute %s: %s\n", 
+	    real_cmdline[0], strerror(errno));
+    exit(1);
+  }
+
+  /* Look for the LSB environment. */
+
+  linker_path = lsb_linker_path;
+  if (access(linker_path, X_OK) == 0) {
+
+    /* We're OK.  Just run the program. */
+
+    execvp(real_cmdline[0], real_cmdline);
+    perror(real_cmdline[0]);
+    exit(1);
+  }
+
+  /* No LSB environment, so we need to run with the native linker.
+     First, let's find it. */
+
+  linker_path = native_linker_path;
+
+  /* Create a new argv array, using the native linker as argv[0]. */
+
+  real_cmdline_len = 0;
+  while (real_cmdline[real_cmdline_len] != NULL)
+    real_cmdline_len++;
+
+  new_cmdline = (char **)malloc(sizeof(char *) * (real_cmdline_len + 1));
+  new_cmdline[0] = linker_path;
+  for (index = 1; index < real_cmdline_len + 1; index++)
+    new_cmdline[index] = real_cmdline[index - 1];
+
+  /* If real_cmdline[0] is not a fully-qualified path, make it so. */
+
+  if (new_cmdline[1][0] != '/') {
+    getcwd(current_dir, PATH_MAX);
+    named_exec = new_cmdline[1];
+    new_cmdline[1] = (char *)malloc(strlen(current_dir) + strlen(named_exec) + 2);
+    sprintf(new_cmdline[1], "%s/%s", current_dir, named_exec);
+  }
+
+  /* Now run the program. */
+
+  execvp(linker_path, new_cmdline);
+  perror(new_cmdline[1]);
+  return 1;
+}
