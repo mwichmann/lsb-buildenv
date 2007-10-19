10b3a1ea038a08f1aab645e74bf45c259340cac5
Mats Wichmann <mats@freestandards.org>
2007-10-19 11:36:42 -0600

    one more header regen

--- headers/X11/Xft/Xft.h
+++ headers/X11/Xft/Xft.h
@@ -11,7 +11,6 @@
 #include <freetype/ftoutln.h>
 #include <freetype/freetype.h>
 #include <freetype/fttypes.h>
-#include <freetype/ftsizes.h>
 #include <freetype/ftimage.h>
 #include <freetype/ftsystem.h>
 
