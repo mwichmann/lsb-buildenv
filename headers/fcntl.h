b10b70dd7bcbbf4f2f57634031641ce99020aac8
Mats Wichmann <mats@freestandards.org>
2007-12-18 08:39:06 -0700

    regen for bugs 1401, 1840

--- headers/fcntl.h
+++ headers/fcntl.h
@@ -10,12 +10,33 @@ extern "C" {
 
 
 
+#if defined __ia64__
+#define O_LARGEFILE	0
+#endif
+#if defined __powerpc64__
+#define O_LARGEFILE	0
+#endif
+#if defined __x86_64__
+#define O_LARGEFILE	0
+#endif
+#if defined __s390x__
+#define O_LARGEFILE	0
+#endif
 #define O_RDONLY	00
 #define O_ACCMODE	0003
 #define O_WRONLY	01
 #define O_CREAT	0100
 #define O_TRUNC	01000
 #define O_SYNC	010000
+#if defined __i386__
+#define O_LARGEFILE	0100000
+#endif
+#if defined __powerpc__ && !defined __powerpc64__
+#define O_LARGEFILE	0100000
+#endif
+#if defined __s390__ && !defined __s390x__
+#define O_LARGEFILE	0100000
+#endif
 #define O_RDWR	02
 #define O_EXCL	0200
 #define O_APPEND	02000
