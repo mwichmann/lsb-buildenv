7783a0ba024333017d9fdef94f52c3dc212ffb87
Mats Wichmann <mats@freestandards.org>
2008-05-06 06:44:34 -0600

    add rpc/rpc.h metaheader (bug 2089)

---
+++ headers/rpc/rpc.h
@@ -0,0 +1,24 @@
+#if (__LSB_VERSION__ >= 11 )
+#ifndef _RPC_RPC_H_
+#define _RPC_RPC_H_
+
+#include <rpc/types.h>
+#include <netinet/in.h>
+#include <rpc/xdr.h>
+#include <rpc/auth.h>
+#include <rpc/clnt.h>
+#include <rpc/rpc_msg.h>
+#include <rpc/auth_unix.h>
+#include <rpc/auth_des.h>
+#include <rpc/svc.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif				/* protection */
+#endif				/* LSB version */
