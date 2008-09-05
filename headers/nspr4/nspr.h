b9cd5336b456d97689bb42818d38e6682647b3b1
Mats Wichmann <mats@freestandards.org>
2008-09-05 08:41:51 -0600

    add nspr meta-header (bug 2286)

---
+++ headers/nspr4/nspr.h
@@ -0,0 +1,26 @@
+#if (__LSB_VERSION__ >= 40 )
+#ifndef _NSPR4_NSPR_H_
+#define _NSPR4_NSPR_H_
+
+#include <nspr4/prclist.h>
+#include <nspr4/prerror.h>
+#include <nspr4/prinit.h>
+#include <nspr4/prinrval.h>
+#include <nspr4/prio.h>
+#include <nspr4/prlock.h>
+#include <nspr4/prmon.h>
+#include <nspr4/prnetdb.h>
+#include <nspr4/prthread.h>
+#include <nspr4/prtime.h>
+#include <nspr4/prtypes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif				/* protection */
+#endif				/* LSB version */
