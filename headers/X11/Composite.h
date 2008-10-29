4a8b0f38c2041676a3f95d993bcc179865abc5f7
Mats Wichmann <mats@freestandards.org>
2008-10-29 11:07:05 -0600

    small tweak to improve devchk problem

--- headers/X11/Composite.h
+++ headers/X11/Composite.h
@@ -2,11 +2,7 @@
 #ifndef _X11_COMPOSITE_H_
 #define _X11_COMPOSITE_H_
 
-#include <X11/Xresource.h>
 #include <X11/Intrinsic.h>
-#include <X11/IntrinsicP.h>
-#include <X11/CompositeP.h>
-#include <X11/CoreP.h>
 
 #ifdef __cplusplus
 extern "C" {
