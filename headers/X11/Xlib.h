e72a12efb79c61b5a5e1790730772d5b31c6fe86
Mats Wichmann <mats@freestandards.org>
2008-11-25 06:46:55 -0700

    add size for XQueryKeymap param (bug 2133)

--- headers/X11/Xlib.h
+++ headers/X11/Xlib.h
@@ -1731,7 +1731,7 @@ extern "C" {
     extern int XQueryExtension(Display *, const char *, int *, int *,
 			       int *);
     extern XFontStruct *XQueryFont(Display *, XID);
-    extern int XQueryKeymap(Display *, char[]);
+    extern int XQueryKeymap(Display *, char[32]);
     extern int XQueryPointer(Display *, Window, Window *, Window *, int *,
 			     int *, int *, int *, unsigned int *);
     extern int XQueryTextExtents(Display *, XID, const char *, int, int *,
