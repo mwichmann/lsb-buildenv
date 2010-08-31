3927c7109be4231877e7964a96bedbbb5e5c4d38
Mats Wichmann <mats@freestandards.org>
2010-08-31 14:10:09 -0600

    add _GTypeCValue definition (bug 3030)

--- headers/glib-2.0/glib-object.h
+++ headers/glib-2.0/glib-object.h
@@ -802,6 +802,14 @@ extern "C" {
 	GSignalFlags run_type;
     };
 
+    union _GTypeCValue {
+	gint v_int;
+	glong v_long;
+	gint64 v_int64;
+	gdouble v_double;
+	gpointer v_pointer;
+    };
+
     struct _GTypeValueTable {
 	void (*value_init) (GValue *);
 	void (*value_free) (GValue *);
