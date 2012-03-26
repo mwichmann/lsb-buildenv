429b5e3e0101721dcbcb8b67b4b204b0808711b5
Jeff Licquia <jeff@licquia.org>
2012-03-26 19:01:36 -0400

    Add gcc 4.5 and 4.6 to the list of recognized gcc versions.

--- lsbdev-cc/lsbcc.c
+++ lsbdev-cc/lsbcc.c
@@ -456,6 +456,8 @@ need_stack_prot_suppression()
   case '2':
   case '3':
   case '4':
+  case '5':
+  case '6':
     /* We pretty much need it for newer versions of 4.x, though here
        we hedge our bets and only test for known gcc versions. */
     return 1;
@@ -531,6 +533,8 @@ need_long_double_64()
   case '2':
   case '3':
   case '4':
+  case '5':
+  case '6':
     /* We pretty much need it for newer versions of 4.x, though here
        we hedge our bets and only test for known gcc versions. */
     return 1;
