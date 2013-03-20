72cf718fd6adbc6876494324dfd94d9e006affb3
Mats Wichmann <mats@linuxfoundation.org>
2013-03-20 13:24:24 -0600

    add crypt_r and friends (bug 3764)

--- headers/crypt.h
+++ headers/crypt.h
@@ -8,6 +8,33 @@ extern "C" {
 #endif
 
 
+#if __LSB_VERSION__ >= 50
+    struct crypt_data {
+	char keysched[128];
+	char sb0[32768];
+	char sb1[32768];
+	char sb2[32768];
+	char sb3[32768];
+	char crypt_3_buf[14];
+	char current_salt[2];
+	long int current_saltbits;
+	int direction;
+	int initialized;
+    };
+
+#endif				/* __LSB_VERSION__ >= 5.0 */
+
+
+/* Function prototypes */
+
+#if __LSB_VERSION__ >= 50
+    extern char *crypt_r(const char *key, const char *salt,
+			 struct crypt_data *data);
+    extern void encrypt_r(const char *block, int edflag,
+			  struct crypt_data *data);
+    extern void setkey_r(const char *key, struct crypt_data *data);
+#endif				/* __LSB_VERSION__ >= 5.0 */
+
 #ifdef __cplusplus
 }
 #endif
--- stub_libs/5.0/S390X/libcrypt.c
+++ stub_libs/5.0/S390X/libcrypt.c
@@ -1,3 +1,6 @@
 void crypt() {} ;
+void crypt_r() {} ;
 void encrypt() {} ;
+void encrypt_r() {} ;
 void setkey() {} ;
+void setkey_r() {} ;
