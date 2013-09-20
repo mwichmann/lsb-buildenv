0fa195feed3fa5016fa7412e69fac838596962b9
Mats Wichmann <mats@linuxfoundation.org>
2013-09-20 05:44:53 -0600

    change some argz parameter names - no behavioral change

--- headers/argz.h
+++ headers/argz.h
@@ -17,16 +17,17 @@ extern "C" {
     extern error_t argz_add(char **argz, size_t * argz_len,
 			    const char *str);
     extern error_t argz_add_sep(char **argz, size_t * argz_len,
-				const char *str, int delim);
+				const char *str, int sep);
     extern error_t argz_append(char **argz, size_t * argz_len,
 			       const char *buf, size_t buf_len);
-    extern size_t argz_count(const char *argz, size_t * len);
+    extern size_t argz_count(const char *argz, size_t * argz_len);
     extern error_t argz_create(char *const argv[], char **argz,
-			       size_t * len);
+			       size_t * argz_len);
     extern error_t argz_create_sep(const char *str, int sep, char **argz,
-				   size_t * len);
+				   size_t * argz_len);
     extern void argz_delete(char **argz, size_t * argz_len, char *entry);
-    extern void argz_extract(const char *argz, size_t len, char **argv);
+    extern void argz_extract(const char *argz, size_t argz_len,
+			     char **argv);
     extern error_t argz_insert(char **argz_insert, size_t * argz_len,
 			       char *before, const char *entry);
     extern char argz_next(const char *argz, size_t argz_len,
@@ -34,7 +35,7 @@ extern "C" {
     extern error_t argz_replace(char **argz, size_t * argz_len,
 				const char *str, const char *with,
 				unsigned int *replace_count);
-    extern void argz_stringify(char *argz, size_t len, int sep);
+    extern void argz_stringify(char *argz, size_t argz_len, int sep);
 #ifdef __cplusplus
 }
 #endif
