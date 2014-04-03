42391c08a79d9aa34ebb56a7b2fe6f08261cf3f5
Mats Wichmann <mats@linuxfoundation.org>
2014-04-03 14:42:48 -0600

    fix GdkStatus (bug 3943)

--- headers/gtk-3.0/gdk/gdk.h
+++ headers/gtk-3.0/gdk/gdk.h
@@ -584,8 +584,13 @@ extern "C" {
 	GDK_MODIFIER_INTENT_SHIFT_GROUP
     } GdkModifierIntent;
 
-    typedef enum
-	GdkStatus;
+    typedef enum {
+	GDK_OK = 0,
+	GDK_ERROR = -1,
+	GDK_ERROR_PARAM = -2,
+	GDK_ERROR_FILE = -3,
+	GDK_ERROR_MEM = -4
+    } GdkStatus;
 
     typedef enum {
 	GDK_GRAB_SUCCESS,
