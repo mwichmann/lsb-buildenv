b0dc323d5dcff1a62fc7a781c89c53a0d1f32a95
Mats Wichmann <mats@linuxfoundation.org>
2014-10-31 11:08:00 -0600

    push POSIX_PRIORITY_SCHEDULING back one LSB release (bug 4035)

--- headers/unistd.h
+++ headers/unistd.h
@@ -82,6 +82,13 @@ extern "C" {
 
 #endif				/* __LSB_VERSION__ >= 2.0 */
 
+#if __LSB_VERSION__ >= 41
+#if __LSB_VERSION__ < 50
+#define _POSIX_PRIORITY_SCHEDULING	200112L
+#endif				/* __LSB_VERSION__ < 5.0 */
+
+#endif				/* __LSB_VERSION__ >= 4.1 */
+
 #if __LSB_VERSION__ >= 50
 #define _POSIX_ASYNC_IO	0
 #define _POSIX2_CHAR_TERM	200809L
